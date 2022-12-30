b0ad553cc91490d80471ee98d90409e8aaf0ea10
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2022-12-30 22:52:09 +0530

    Create array introduction.cpp

---
+++ Assignment 1/array introduction.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int i=0;
+    int j = n-1;
+    int temp;
+    while(i<j){
+        temp = arr[i];
+        arr[i]= arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+
+    for(int i =0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
