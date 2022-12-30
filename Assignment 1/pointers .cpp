3cb603347d00b55e91c17f6352f2e3d8f489996d
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2022-12-30 23:00:42 +0530

    Create pointers .cpp

---
+++ Assignment 1/pointers .cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+
+    int sum = *a + *b;
+    int sub = *a - *b;
+    if(sub<0){
+        sub*= (-1);
+    }
+
+    *a = sum;
+    *b = sub;
+
+
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
