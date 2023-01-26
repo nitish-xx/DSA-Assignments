c8c2416b72bac21d30d4a645bb7b4d5a40649d48
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2023-01-27 00:25:58 +0530

    Create leetcode preOrder using stack.cpp

---
+++ Assignment 10/leetcode preOrder using stack.cpp
@@ -0,0 +1,26 @@
+leetcode.com/problems/binary-tree-preorder-traversal/submissions/885848603/
+
+class Solution {
+public:
+      vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty()){
+            root=s.top();
+            s.pop();
+            ans.push_back(root->val);
+
+            if(root->right!=NULL){
+                s.push(root->right);
+            }
+            if(root->left!=NULL){
+                s.push(root->left);
+            }
+        }
+        return ans;
+    }
+};
