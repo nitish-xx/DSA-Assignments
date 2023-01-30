6cf0bd9bf6b5bba0065c216729d11399a4647c02
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2023-01-30 10:04:27 +0530

    Create hackerrank  level order traversal.cpp

---
+++ Assignment 10/hackerrank  level order traversal.cpp
@@ -0,0 +1,16 @@
+https://www.hackerrank.com/challenges/tree-level-order-traversal/problem?isFullScreen=true
+
+void levelOrder(Node * root) {
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            
+        Node*root = q.front();
+        q.pop();            
+        cout<< root->data<<" ";
+        if(root->left)q.push(root->left);
+        if(root->right) q.push(root->right);
+        
+        }
+   }
